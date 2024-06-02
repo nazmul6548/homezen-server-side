ae9a0b1c156c8b70b2aab50ea9615f156bc57ad6
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-06-02 11:13:26 +0600

    add property data feching

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000
 
 // middleware
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5176'],
+    origin: [ 'http://localhost:5176'],
     credentials: true,
     optionSuccessStatus: 200,
   }
@@ -18,7 +18,7 @@ const corsOptions = {
 
 
 
-  const { MongoClient, ServerApiVersion } = require('mongodb');
+  const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
   const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ce00xrg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
   
   // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,12 +33,33 @@ const corsOptions = {
   async function run() {
     try {
      const houseCollection = client.db("realEstatePlatform").collection("house")
+     const reviewCollection = client.db("realEstatePlatform").collection("reviews")
 // get all house
      app.get("/house", async (req, res) => {
         const result = await houseCollection.find().toArray();
         res.send(result);
       });
-      await client.db("admin").command({ ping: 1 });
+    //   post property
+    app.post("/house",async (req,res)=>{
+        const property = req.body;
+        const result = await houseCollection.insertOne(property)
+        res.send(result)
+    })
+    //   details for house
+     app.get("/house/:id", async (req, res) => {
+        const  id  = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await houseCollection.findOne(query);
+        res.send(result);
+      });
+
+    //   reviews
+    app.get("/reviews", async (req, res) => {
+        const result = await reviewCollection.find().toArray();
+        res.send(result);
+      });
+    
+    //   await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
       
