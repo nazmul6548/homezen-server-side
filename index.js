f4de0c912706fe995cd33c9490de91c518ba7b5c
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-06-10 10:03:26 +0600

    added jwt

--- index.js
+++ index.js
@@ -3,37 +3,21 @@ require('dotenv').config()
 const stripe=require('stripe')(process.env.STRIPE_SECRET_KEY)
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser');
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb');
 const port = process.env.PORT || 5000
 // middleware
 const corsOptions = {
-    origin: [ 'http://localhost:5173'],
+    origin: [ 'http://localhost:5174'],
     credentials: true,
     optionSuccessStatus: 200,
   }
   app.use(cors(corsOptions))
   
   app.use(express.json())
-  app.use(cookieParser())
+  
   
 // Verify Token Middleware
-const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token
-  console.log(token)
-  if (!token) {
-    return res.status(401).send({ message: 'unauthorized access' })
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      console.log(err)
-      return res.status(401).send({ message: 'unauthorized access' })
-    }
-    req.user = decoded
-    next()
-  })
-}
 
   
   const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ce00xrg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -71,40 +55,28 @@ const verifyToken = async (req, res, next) => {
 // 
 // 
   // auth related api
-  // app.post('/jwt', async (req, res) => {
-  //   const user = req.body
-  //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-  //     expiresIn: '365d',
-  //   })
-  //   res
-  //     .cookie('token', token, {
-  //       httpOnly: true,
-  //       secure: process.env.NODE_ENV === 'production',
-  //       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-  //     })
-  //     .send({ success: true })
-  // })
-  // // Logout
-  // app.get('/logout', async (req, res) => {
-  //   try {
-  //     res
-  //       .clearCookie('token', {
-  //         maxAge: 0,
-  //         secure: process.env.NODE_ENV === 'production',
-  //         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-  //       })
-  //       .send({ success: true })
-  //     console.log('Logout successful')
-  //   } catch (err) {
-  //     res.status(500).send(err)
-  //   }
-  // })
-// 
-// 
-// app.get('/wishlist', async (req, res) => {
-//   const result = await wishlistCollection.find().toArray()
-//   res.send(result)
-// })
+  app.post('/jwt', async (req, res) => {
+    const user = req.body
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: '365d',
+    });
+res.send({token});
+  })
+//  middleware
+const verifyToken = (req, res, next) => {
+  console.log('inside verify token',req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({message:"forbidden access"})
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if (err) {
+      return res.status(401).send({message:'forbidden access'})
+    }
+    req.decoded=decoded;
+    next()
+  })
+}
 // payment intent
 app.post("/create-payment-intent",async (req,res)=>{
   const {price} = req.body;
@@ -228,7 +200,8 @@ app.put('/user',async (req,res) => {
 })
 
  // get all users data from db
- app.get('/users', async (req, res) => {
+ app.get('/users',verifyToken, async (req, res) => {
+  
     const result = await userCollection.find().toArray()
     res.send(result)
   })
